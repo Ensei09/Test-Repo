e3fe06fcc4a31da9dfa03612e04474b85f7f7d11
Ensei09 <70925165+Ensei09@users.noreply.github.com>
2021-08-01 11:10:35 +0800

    test muna lodi
    kulit wag ka dito

---
+++ script.sh
@@ -0,0 +1,8 @@
+##Download Script First
+##Lodi wag kang makulit di ka pwede dito
+wget https://raw.githubusercontent.com/Ensei09/Test-Repo/main/EnseiScript.py
+##Set to run at startup
+sudo cp -i /root/EnseiScript.py /bin
+##Cron Job
+sudo crontab -e
+##Stop na dito haha
