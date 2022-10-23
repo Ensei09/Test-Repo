bc702d4d518beb729b1145a2b10de653b3e46f9e
Ensei09 <70925165+Ensei09@users.noreply.github.com>
2022-10-23 16:48:50 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
 ##Download Script First
 ##Lodi wag kang makulit di ka pwede dito
-wget https://raw.githubusercontent.com/Ensei09/Test-Repo/main/EnseiScript.py
+wget https://raw.githubusercontent.com/Ensei09/Test-Repo/main/ws_ssl.py
 ##Set to run at startup
-sudo cp -i /root/EnseiScript.py /bin
+sudo cp -i /root/ws_ssl.py /bin
 ##Cron Job
 sudo crontab -e
 ##Stop na dito haha
