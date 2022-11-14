392b30bdc0a1e559f44bd3889d3dc1ff330d81d8
Ensei09 <70925165+Ensei09@users.noreply.github.com>
2022-11-14 23:08:13 +0800

    Update private.sh

--- YPanel Dependencies/private.sh
+++ YPanel Dependencies/private.sh
@@ -2,10 +2,10 @@
 error_reporting(E_ERROR | E_PARSE);
 ini_set('display_errors', '1');
 
-$DB_host = '127.0.0.1';
-$DB_user = 'enseivpn';
-$DB_pass = 'enseivpn111422';
-$DB_name = 'enseivpn';
+$DB_host = "18.138.215.77";
+$DB_user = "enseivpn";
+$DB_pass = "ajyDPYthB8FTA25G";
+$DB_name = "enseivpn";
 
 $mysqli = new MySQLi($DB_host,$DB_user,$DB_pass,$DB_name);
 if ($mysqli->connect_error) {
