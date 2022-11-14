5955110e5831ad38d6d72a077091d0bb8188cc17
Ensei09 <70925165+Ensei09@users.noreply.github.com>
2022-11-14 09:53:00 +0800

    Create aapanel.sh

---
+++ aapanel.sh
@@ -0,0 +1,7 @@
+## Initialize system to be up to date
+sudo apt update
+sudo apt upgrade
+## Install aaPanel
+wget -O install.sh http://www.aapanel.com/script/install-ubuntu_6.0_en.sh && sudo bash install.sh
+## Do you want to install aaPanel to the /www directory now?(y/n): y
+## Do you need to enable the panel SSl ? (yes/n): n
